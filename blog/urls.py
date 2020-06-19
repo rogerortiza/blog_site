69d22aaaf765fae6807114ed7d7e854d15491c92
rogerortiza <rortiz@contractroom.com>
2020-06-19 17:52:24 -0500

    adding full text search for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .feeds import LatestPostsFeed
 from .views import *
 
 app_name = 'blog'
@@ -7,5 +8,7 @@ urlpatterns = [
     path('', post_list, name='post_list'),
     path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
-    path('<int:post_id>/share/', post_share, name='post_share')
+    path('<int:post_id>/share/', post_share, name='post_share'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', post_search, name='post_search'),
 ]
