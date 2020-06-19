16a25f8800a68845e1ac4b89f97e960e0e791673
rogerortiza <rortiz@contractroom.com>
2020-06-18 22:10:03 -0500

    blog site

---
+++ blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import *
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', post_list, name='post_list'),
+    path('tag/<slug:tag_slug>/', post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', post_detail, name='post_detail'),
+    path('<int:post_id>/share/', post_share, name='post_share')
+]
